5dfa86158ee4d380b2078dfc4d6815995345386d
SoulRvr <pawelc29@interia.pl>
2023-07-29 14:12:06 +0200

    working chart with hover

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
+import chartData from "./data.json";
+
 const App = () => {
+  const days = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+  let currentDay = days[new Date().getDay()];
+
   return (
     <main>
       <h2>Spending - Last 7 days</h2>
       <div className="chart">
         <ul>
-          <li>mon</li>
-          <li>tue</li>
-          <li>wed</li>
-          <li>thu</li>
-          <li>fri</li>
-          <li>sat</li>
-          <li>sun</li>
+          {chartData.map((data) => (
+            <li key={data.day}>
+              <div className="amount">${data.amount}</div>
+              <div
+                className={`bar ${data.day == currentDay && "currentDay"}`}
+                style={{
+                  maxHeight: `${data.amount * 3}px`,
+                }}
+              ></div>
+              {data.day}
+            </li>
+          ))}
         </ul>
       </div>
       <div className="summary">
